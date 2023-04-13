fa1193202712ef64b4a1e7da650f0efc645d0566
NgocTuan01 <106262125+NgocTuan01@users.noreply.github.com>
2023-04-13 11:38:15 +0700

    up_c2

---
+++ app/src/main/java/com/example/ballapp/InformatinonTeam/TeamInformationRepository.kt
@@ -0,0 +1,57 @@
+package com.example.ballapp.InformatinonTeam
+
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
+import java.io.File
+import javax.inject.Inject
+
+class TeamInformationRepository @Inject constructor(
+    private val firebaseDatabase: FirebaseDatabase,
+) {
+
+    fun loadTeamInfo(
+        userUID: String,
+        onSuccess: (DataSnapshot) -> Unit,
+        onFail: (Exception) -> Unit,
+    ) {
+        firebaseDatabase.getReference("Teams").child(userUID).get()
+            .addOnSuccessListener {
+                onSuccess(it)
+            }
+            .addOnFailureListener {
+                onFail(it)
+            }
+    }
+
+    fun updateTeamInfo(
+        teamUid: String,
+        teamName: String,
+        teamLocation: String,
+        teamPeopleNumber: String,
+        teamNote: String,
+        deviceToken: String,
+        onSuccess: (String) -> Unit,
+        onFail: (String) -> Unit,
+    ) {
+        val updateTeams = mapOf(
+            "teamUid" to teamUid,
+            "teamName" to teamName,
+            "teamLocation" to teamLocation,
+            "teamNote" to teamNote,
+            "teamPeopleNumber" to teamPeopleNumber,
+            "deviceToken" to deviceToken
+        )
+
+        firebaseDatabase.getReference("Teams").child(teamUid).updateChildren(updateTeams)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    onSuccess(it.toString())
+                } else {
+                    onFail(it.exception?.message.orEmpty())
+                }
+            }
+            .addOnFailureListener {
+                onFail(it.message.orEmpty())
+            }
+    }
+}
